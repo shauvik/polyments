699f7042e08d21202dd193a569a44be54b48d571
Shauvik Roy Choudhary <shauvik@gmail.com>
2016-01-10 02:00:58 -0500

    synced polyments

---
+++ refresh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf bower_components
+
+bower install Polymer/polymer
+bower install PolymerElements/iron-elements
+bower install PolymerElements/paper-elements
+bower install PolymerElements/gold-elements
+bower install PolymerElements/neon-elements
+bower install PolymerElements/platinum-elements
+bower install GoogleWebComponents/google-web-components
+bower install PolymerElements/molecules
+
